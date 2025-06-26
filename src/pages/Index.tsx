ea8f2f39864bc4cebeef567cf351e2ac765cb671
kibet-dennis <dennissebastien4136@gmail.com>
2025-06-26 21:33:15 +0300

    link photography

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -163,7 +163,7 @@ const Index = () => {
     category: "Digital Strategy",
     tools: [
       { name: "Our Process", link: "https://kibet-dennis.github.io/strategy./" },
-      { name: "Photography/Videography", link: "https://www.adobe.com/products/premiere.html" },
+      { name: "Photography/Videography", link: "https://kibet-dennis.github.io/videography/" },
 
     ],
     thumbnail: "https://i.postimg.cc/hvGJLw9g/Gemini-Generated-Image-nuohtenuohtenuoh.png"
